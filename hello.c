710941514ca70203e67b15a4ce73e664f06fa395
Alvaro Moreno <alvaritotennis@gmail.com>
2018-11-22 10:26:39 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -7,6 +7,10 @@ int main()
 {
   printf("My name is Victor Moreno Lozano\n");
   printf("My major is Computer Engineering\n");
+  
+  printf("My name is Alvaro Moreno Lozano\n");
+  printf("My major is Biological Systems Engineering\n");
+
 
   return 0;
 }
